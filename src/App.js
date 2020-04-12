2b674ee8cb4eab09dfc036c240d397698a867da7
John Herbener <john@herbener.cloud>
2020-04-12 16:48:02 -0400

    login section

--- src/App.js
+++ src/App.js
@@ -1,28 +1,102 @@
-import React from "react";
-import PageWrapper from "./components/PageWrapper";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react'
+import PageWrapper from './components/PageWrapper'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 // Pages
-import Home from "./components/Pages/Home";
-import About from "./components/Pages/About";
-import Contact from "./components/Pages/Contact";
-import Services from "./components/Pages/Services";
-import Portfolio from "./components/Pages/Portfolio";
-import Team from "./components/Common/Team";
+import Home from './components/Pages/Home'
+import About from './components/Pages/About'
+import Contact from './components/Pages/Contact'
+import Services from './components/Pages/Services'
+import Portfolio from './components/Pages/Portfolio'
+import Team from './components/Common/Team'
+import Login from './components/Pages/Login'
 
-function App() {
-  return (
-    <Router>
-      <PageWrapper>
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/about" component={About} exact={true} />
-        <Route path="/contact" component={Contact} exact={true} />
-        <Route path="/services" component={Services} exact={true} />
-        <Route path="/portfolio" component={Portfolio} exact={true} />
-        <Route path="/team" component={Team} exact={true} />
-      </PageWrapper>
-    </Router>
-  );
+import Dashboard from './components/Pages/Dashboard'
+import AdminWrapper from './components/AdminWrapper'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route
+            path='/admin'
+            render={(props) => (
+              <AdminWrapper>
+                {this.props.auth.token ? <Dashboard /> : <Login />}
+              </AdminWrapper>
+            )}
+          />
+
+          <Route
+            path='/'
+            exact={true}
+            render={(props) => (
+              <PageWrapper>
+                <Home {...props} />
+              </PageWrapper>
+            )}
+          />
+          <Route
+            path='/about'
+            exact={true}
+            render={(props) => (
+              <PageWrapper>
+                <About {...props} />
+              </PageWrapper>
+            )}
+          />
+          <Route
+            path='/contact'
+            exact={true}
+            render={(props) => (
+              <PageWrapper>
+                <Contact {...props} />
+              </PageWrapper>
+            )}
+          />
+          <Route
+            path='/services'
+            exact={true}
+            render={(props) => (
+              <PageWrapper>
+                <Services {...props} />
+              </PageWrapper>
+            )}
+          />
+          <Route
+            path='/portfolio'
+            exact={true}
+            render={(props) => (
+              <PageWrapper>
+                <Portfolio {...props} />
+              </PageWrapper>
+            )}
+          />
+          <Route
+            path='/team'
+            exact={true}
+            render={(props) => (
+              <PageWrapper>
+                <Team {...props} />
+              </PageWrapper>
+            )}
+          />
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
